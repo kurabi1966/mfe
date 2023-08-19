24844b456ea92bf6a019939990dd6fb6d8126340
Mohammed Ammar Kurabi <kurabi@gmail.com>
2023-08-19 20:31:58 +0400

    Small change to rerun the github action

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -3,7 +3,7 @@ import MarketingApp from "./components/MarketingApp";
 export default () => {
   return (
     <div>
-      <h1>Container MFE</h1>
+      <h1>Container MFE.</h1>
       <hr />
       <MarketingApp />
     </div>
