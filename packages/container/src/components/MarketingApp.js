7cce53307bd9902967164521e0f5128e5059155b
Mohammed Ammar Kurabi <kurabi@gmail.com>
2023-08-19 14:06:08 +0400

    Initial commit

---
+++ packages/container/src/components/MarketingApp.js
@@ -0,0 +1,11 @@
+import React, { useRef, useEffect } from "react";
+
+import { mount as mountMarketing } from "marketing/MarketingApp";
+
+export default () => {
+  const ref = useRef(null);
+  useEffect(() => {
+    mountMarketing(ref.current);
+  });
+  return <div ref={ref}></div>;
+};
