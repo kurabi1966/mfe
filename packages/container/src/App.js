d1b64c33deca681156d984636fbf66f905bfa0e5
Mohammed Ammar Kurabi <kurabi@gmail.com>
2023-08-20 20:00:02 +0400

    Fixed the styleing issue

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,15 +1,22 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from "@material-ui/core/styles";
+const generateClassName = createGenerateClassName({ productionPrefix: "con" });
 
 import MarketingApp from "./components/MarketingApp";
 import Header from "./components/Header";
 export default () => {
   return (
-    <BrowserRouter>
-      <div>
-        <Header />
-        <MarketingApp />
-      </div>
-    </BrowserRouter>
+    <StylesProvider generateClassName={generateClassName}>
+      <BrowserRouter>
+        <div>
+          <Header />
+          <MarketingApp />
+        </div>
+      </BrowserRouter>
+    </StylesProvider>
   );
 };
